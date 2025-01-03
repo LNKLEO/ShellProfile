0b2ff8efaf99eae302924a0f2a77dc00e3bbb0c4
LNKLEO <LinkeyLeo@outlook.com>
2025-01-03 10:34:24 +0800

    Add NEOVIM

---
+++ LAZYVIM/pkg-cache.lua
@@ -0,0 +1,10 @@
+return {version=12,pkgs={{source="lazy",dir="C:/Users/LNKLEO/AppData/Local/nvim-data/lazy/noice.nvim",spec=function()
+return {
+  -- nui.nvim can be lazy loaded
+  { "MunifTanjim/nui.nvim", lazy = true },
+  {
+    "folke/noice.nvim",
+  },
+}
+
+end,file="lazy.lua",name="noice.nvim",},{source="lazy",dir="C:/Users/LNKLEO/AppData/Local/nvim-data/lazy/plenary.nvim",spec={"nvim-lua/plenary.nvim",lazy=true,},file="community",name="plenary.nvim",},{source="rockspec",dir="C:/Users/LNKLEO/AppData/Local/nvim-data/lazy/telescope.nvim",spec={"telescope.nvim",specs={{"nvim-lua/plenary.nvim",lazy=true,},},build=false,},file="telescope.nvim-scm-1.rockspec",name="telescope.nvim",},},}
