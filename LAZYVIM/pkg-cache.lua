541fa401b53bc4f9332fb0c1732cdc9efa91d089
LNKLEO <LinkeyLeo@outlook.com>
2025-02-21 15:26:16 +0800

    update nvim

--- LAZYVIM/pkg-cache.lua
+++ LAZYVIM/pkg-cache.lua
@@ -1,4 +1,4 @@
-return {version=12,pkgs={{source="lazy",dir="C:/Users/LNKLEO/AppData/Local/nvim-data/lazy/noice.nvim",spec=function()
+return {version=12,pkgs={{source="lazy",dir="C:/Users/LNKLEO/AppData/Local/nvim-data/lazy/noice.nvim",file="lazy.lua",spec=function()
 return {
   -- nui.nvim can be lazy loaded
   { "MunifTanjim/nui.nvim", lazy = true },
@@ -7,4 +7,4 @@ return {
   },
 }
 
-end,file="lazy.lua",name="noice.nvim",},{source="lazy",dir="C:/Users/LNKLEO/AppData/Local/nvim-data/lazy/plenary.nvim",spec={"nvim-lua/plenary.nvim",lazy=true,},file="community",name="plenary.nvim",},{source="rockspec",dir="C:/Users/LNKLEO/AppData/Local/nvim-data/lazy/telescope.nvim",spec={"telescope.nvim",specs={{"nvim-lua/plenary.nvim",lazy=true,},},build=false,},file="telescope.nvim-scm-1.rockspec",name="telescope.nvim",},},}
+end,name="noice.nvim",},{source="lazy",dir="C:/Users/LNKLEO/AppData/Local/nvim-data/lazy/plenary.nvim",file="community",spec={"nvim-lua/plenary.nvim",lazy=true,},name="plenary.nvim",},{source="rockspec",dir="C:/Users/LNKLEO/AppData/Local/nvim-data/lazy/telescope.nvim",file="telescope.nvim-scm-1.rockspec",spec={"telescope.nvim",build=false,specs={{"nvim-lua/plenary.nvim",lazy=true,},},},name="telescope.nvim",},},}
